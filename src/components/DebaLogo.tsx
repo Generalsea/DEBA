53aae413ee40619894d7d6f6de0caf4768b4775d
TM-2027 <51057584+Generalsea@users.noreply.github.com>
2026-09-24 00:27:16 +0300

    fix(brand): render logo with native image element

--- src/components/DebaLogo.tsx
+++ src/components/DebaLogo.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 type Props = {
@@ -11,13 +10,13 @@ type Props = {
 export default function DebaLogo({ href = '/', compact = false, className = '', title = 'DEBA' }: Props) {
   const content = (
     <span className={'deba-logo-lockup ' + (compact ? 'is-compact ' : '') + className}>
-      <Image
+      <img
         src="/brand/deba-logo.png"
         alt={title}
         width={180}
         height={60}
-        priority
         className="deba-logo-image"
+        decoding="async"
       />
     </span>
   )
