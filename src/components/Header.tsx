b211bbb2c96a058042906e5adec9d3d6774d6a13
TM-2027 <51057584+Generalsea@users.noreply.github.com>
2026-09-25 19:50:05 +0300

    test: add stable marketplace search selector

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -508,6 +508,7 @@ export default function Header({
                 </div>
 
                 <input
+                  data-testid="marketplace-search"
                   name="q"
                   value={query}
                   onChange={(event) => {
