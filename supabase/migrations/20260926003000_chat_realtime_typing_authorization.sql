c63816874f2f23a8df99617f1d7e12b2ae75e064
TM-2027 <51057584+Generalsea@users.noreply.github.com>
2026-09-26 00:28:31 +0300

    feat: authorize private chat typing broadcasts

---
+++ supabase/migrations/20260926003000_chat_realtime_typing_authorization.sql
@@ -0,0 +1,30 @@
+-- DEBA Realtime typing authorization
+--
+-- Typing indicators are transient Broadcast events on a private room channel.
+-- Access is limited to authenticated members of the corresponding chat room.
+
+create policy "deba chat participants can receive broadcast"
+on realtime.messages
+for select
+to authenticated
+using (
+  extension = 'broadcast'
+  and case
+    when realtime.topic() ~ '^deba-chat:[0-9a-fA-F-]{36}$'
+      then private.is_chat_participant(substring(realtime.topic() from 11)::uuid)
+    else false
+  end
+);
+
+create policy "deba chat participants can send broadcast"
+on realtime.messages
+for insert
+to authenticated
+with check (
+  extension = 'broadcast'
+  and case
+    when realtime.topic() ~ '^deba-chat:[0-9a-fA-F-]{36}$'
+      then private.is_chat_participant(substring(realtime.topic() from 11)::uuid)
+    else false
+  end
+);
